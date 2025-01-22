7d86c5f92a16dd3c4652dbc3aed9cf47d9253784
Khidir Afwan Amlabar <102141969+khidir05@users.noreply.github.com>
2025-01-22 23:34:34 +0700

    New Upd 22/01/25

---
+++ rewriteAppsUsingGithub/src/main/java/com/rewritetext/RewriteTextApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rewriteappsusinggithub;
+
+/**
+ *
+ * @author amlab
+ */
+public class RewriteAppsUsingGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
